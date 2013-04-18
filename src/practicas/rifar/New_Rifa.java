b59c8dbb6bb5d893c6bf254fa99416fc97819df6
Fidel Arias <086pako@gmail.com>
2013-04-18 18:47:51 -0400

    Desde el inicio

---
+++ src/practicas/rifar/New_Rifa.java
@@ -0,0 +1,25 @@
+package practicas.rifar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class New_Rifa extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_new_rifa);
+	}
+	
+	public void cancelar1(View view) 
+	{
+        finish();
+	}
+	
+	public void redFecha(View view)
+	{
+	}
+}
---
+++ src/practicas/rifar/New_Ticket.java
@@ -0,0 +1,14 @@
+package practicas.rifar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class New_Ticket extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_new_ticket);
+	}
+}
